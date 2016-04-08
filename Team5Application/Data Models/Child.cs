df5f8b97a20291a42526b7c52ca662da6de0dec3
elmo911 <jacobmwolfe@gmail.com>
2016-04-08 18:59:20 -0500

    Models

---
+++ Team5Application/Data Models/Child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team5Application.Data_Models
+{
+    public class Child
+    {
+        String firstName { get; set; }
+        String lastName { get; set; }
+        int Age { get; set; }
+
+    }
+}
